a259b55b070b399feab059ecb1bac58e127c1d83
SamTheCoder445 <u18100156@tuks.co.za>
2026-05-14 03:01:14 +0200

    feat: add ApproveRequest DTO

---
+++ backend/src/main/java/com/hackathon/platform/dto/ApproveRequest.java
@@ -0,0 +1,10 @@
+package com.hackathon.platform.dto;
+
+public class ApproveRequest {
+    private boolean approve;
+
+    public ApproveRequest() {}
+
+    public boolean isApprove() { return approve; }
+    public void setApprove(boolean approve) { this.approve = approve; }
+}
