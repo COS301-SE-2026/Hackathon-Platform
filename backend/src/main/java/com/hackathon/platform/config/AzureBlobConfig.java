605571be7dcd30e21eeca8c2f7ffcfe4d03082f1
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2026-05-15 13:25:47 +0200

    Revert "Azure blob config"
    This reverts commit 89a372b6b20d5f55d3f546dad8bfa1cf36518266.

--- backend/src/main/java/com/hackathon/platform/config/AzureBlobConfig.java
+++ backend/src/main/java/com/hackathon/platform/config/AzureBlobConfig.java
@@ -1,33 +1,12 @@
-package com.hackathon.platform.config;
+package java.com.hackathon.platform.config;
 
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Spring configuration for Azure Blob Storage.
- */
-@Configuration
-@Getter
+
 public class AzureBlobConfig {
-
-  @Value("${azure.storage.connection-string}")
-  private String connectionString;
-
-  @Value("${azure.storage.containers.event-resources}")
-  private String eventResourcesContainer;
-
-  @Value("${azure.storage.containers.submissions}")
-  private String submissionsContainer;
-
-  @Value("${azure.storage.containers.scoring-logs}")
-  private String scoringLogsContainer;
-
-  @Value("${azure.storage.sas-expiry-minutes:60}")
-  private int sasExpiryMinutes;
-
- 
+    
 }
