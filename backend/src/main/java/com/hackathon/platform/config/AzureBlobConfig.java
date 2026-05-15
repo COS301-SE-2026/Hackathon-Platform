1fc525a5308fbd0b1a5ac9cec4cba755eb16198b
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2026-05-15 13:28:58 +0200

    Restore AzureBlobConfig.java to b08e931 state

--- backend/src/main/java/com/hackathon/platform/config/AzureBlobConfig.java
+++ backend/src/main/java/com/hackathon/platform/config/AzureBlobConfig.java
@@ -1,12 +1,4 @@
-package java.com.hackathon.platform.config;
-
-import com.azure.storage.blob.BlobServiceClient;
-import com.azure.storage.blob.BlobServiceClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+package com.hackathon.platform.config;
 
 public class AzureBlobConfig {
-    
 }
