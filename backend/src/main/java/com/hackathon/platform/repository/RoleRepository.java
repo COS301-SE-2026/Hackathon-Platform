32c1b5f2fb510c424aa15b4e8a9bb3f43b9b71ab
ayush-635 <ayushsanjith@gmail.com>
2026-05-15 02:22:53 +0200

    Created RoleRepository

---
+++ backend/src/main/java/com/hackathon/platform/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.hackathon.platform.repository;
+
+import com.hackathon.platform.model.Role;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+ 
+  Optional<Role> findByName(String name);
+}
