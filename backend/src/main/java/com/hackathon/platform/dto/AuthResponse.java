25ef22e1d95f37726c52e12ea1f672e190df736e
ayush-635 <ayushsanjith@gmail.com>
2026-05-15 02:56:08 +0200

    auth response DTO

--- backend/src/main/java/com/hackathon/platform/dto/AuthResponse.java
+++ backend/src/main/java/com/hackathon/platform/dto/AuthResponse.java
@@ -1,4 +1,19 @@
 package com.hackathon.platform.dto;
 
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class AuthResponse {
+
+  private String token;
+  private UUID userId;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String role;
 }
