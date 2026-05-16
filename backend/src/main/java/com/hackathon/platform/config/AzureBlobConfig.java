16bf92a22b26fdd460fe1ecebe9d2ce769e6a612
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2026-05-16 13:05:00 +0200

    added blob config imports

--- backend/src/main/java/com/hackathon/platform/config/AzureBlobConfig.java
+++ backend/src/main/java/com/hackathon/platform/config/AzureBlobConfig.java
@@ -1,4 +1,13 @@
 package com.hackathon.platform.config;
 
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
 public class AzureBlobConfig {
 }
