2a5e319a8cc0e43c5d8b581386233427fb647519
REDD-debug <u23558572@tuks.co.za>
2026-05-16 15:02:14 +0200

    fixed admin routing and folder structure to display admin shell properly

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-
+import { AdminShellComponent } from './admin/admin-shell.component/admin-shell.component';
 export const routes: Routes = [
   {
     path: '',
@@ -11,10 +11,33 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/auth/login/login.component').then(m => m.LoginComponent),
   },
-   {
+  {
     path: 'register',  
     loadComponent: () =>
       import('./features/auth/register/register.component').then(m => m.RegisterComponent),
+  },
+  {
+    path: 'admin',
+    component: AdminShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./admin/dashboard/dashboard.component').then(m => m.DashboardComponent),
+      },
+      {
+        path: 'create-event',
+        loadComponent: () => import('./admin/create-event/createEvent.component').then(m => m.CreateEventComponent),
+      },
+      {
+        path: 'event-list',
+        loadComponent: () => import('./admin/event-list/eventlist.component').then(m => m.EventlistComponent),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      }
+    ]
   },
   {
     path: '**',
