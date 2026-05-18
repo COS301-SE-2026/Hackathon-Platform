6b3f44ba076d6b00f972c651f22d9e857da80304
HeinrichWRomer <heinrichwillemromer101@gmail.com>
2026-05-18 17:20:24 +0200

    create a test user for integration testing

--- backend/src/test/java/com/hackathon/platform/controller/AuthControllerTest.java
+++ backend/src/test/java/com/hackathon/platform/controller/AuthControllerTest.java
@@ -1,3 +1,34 @@
 package com.hackathon.platform.controller;
 
-public class AuthControllerTest { }
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hackathon.platform.dto.AuthResponse;
+import com.hackathon.platform.dto.RegisterRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+@Transactional
+class AuthControllerTest {
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objMapper;
+
+    private RegisterRequest validRequest;
+
+    @BeforeEach
+    void setUp() {
+        validRequest = new RegisterRequest("Jane", "Doe", "Jane.Doe@gmail.com", "TestPassword");
+    }
+}
