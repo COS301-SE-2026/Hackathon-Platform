12e0949b49c1e9515660f9279bca000f9ab220da
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2026-05-26 18:53:07 +0200

    Added LevelFile model

---
+++ backend/src/main/java/com/hackathon/platform/model/LevelFile.java
@@ -0,0 +1,17 @@
+// src/main/java/com/hackathon/platform/model/LevelFile.java
+package com.hackathon.platform.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+
+@Entity
+@Table(name = "levelfiles", schema = "public")
+public class LevelFile {
+
+
+}
