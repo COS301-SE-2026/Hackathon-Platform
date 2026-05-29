cd8317b524c2ce0dd9d124d4ecd8b36c1ad81e9b
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2026-05-29 16:42:19 +0200

    Created SolverVersion model

---
+++ backend/src/main/java/com/hackathon/platform/model/SolverVersion.java
@@ -0,0 +1,16 @@
+package com.hackathon.platform.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "solverversion", schema = "public")
+public class SolverVersion {
+
+}
