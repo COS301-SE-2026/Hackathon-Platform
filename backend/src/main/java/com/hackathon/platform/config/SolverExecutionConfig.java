c56286973529fab77848c42915c1bc61f819b8a0
Jasveer-Ramdhaw <Ramdhawjasveer@gmail.com>
2026-06-22 10:07:27 +0200

    created Solver execution config

---
+++ backend/src/main/java/com/hackathon/platform/config/SolverExecutionConfig.java
@@ -0,0 +1,15 @@
+package com.hackathon.platform.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+
+//Configuration for hardened solver execution
+ 
+@Configuration
+@Getter
+public class SolverExecutionConfig {
+
+
+}
