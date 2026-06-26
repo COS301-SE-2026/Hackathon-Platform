afbda27f80741e0419b05e401f301067ab757072
ayush-635 <ayushsanjith@gmail.com>
2026-06-26 21:06:04 +0200

    Changed levels and solver FK

--- backend/src/main/resources/db/migration/V5__add_hackathon.sql
+++ backend/src/main/resources/db/migration/V5__add_hackathon.sql
@@ -11,14 +11,42 @@ ADD COLUMN hackathon_id UUID NOT NULL;
 ALTER TABLE events
 ADD CONSTRAINT fk_event_hackathon
 FOREIGN KEY (hackathon_id)
-REFERENCES hackathon(hackathon_id)
+REFERENCES hackathon(hackathon_id);
 
 ALTER TABLE events
-DROP CONSTRAINT chk_events_duration;
+DROP CONSTRAINT chk_events_status;
 
 ALTER TABLE events
-ADD CONSTRAINT chk_events_duration
+ADD CONSTRAINT chk_events_status
 CHECK (status IN ('ACTIVE', 'INACTIVE', 'COMPLETED', 'CANCELED', 'DRAFT'));
 
 ALTER TABLE levelfiles
 DROP COLUMN file_type;
+
+ALTER TABLE levels
+DROP CONSTRAINT fk_levels_event_id;
+
+ALTER TABLE levels
+DROP COLUMN event_id;
+
+ALTER TABLE levels
+ADD COLUMN hackathon_id UUID NOT NULL;
+
+ALTER TABLE levels
+ADD CONSTRAINT fk_levels_hackathon_id
+FOREIGN KEY (hackathon_id)
+REFERENCES hackathon(hackathon_id);
+
+ALTER TABLE solverversion
+DROP CONSTRAINT fk_solverversion_event_id;
+
+ALTER TABLE solverversion
+DROP COLUMN event_id;
+
+ALTER TABLE solverversion
+ADD COLUMN hackathon_id UUID NOT NULL;
+
+ALTER TABLE solverversion
+ADD CONSTRAINT fk_solverversion_hackathon_id
+FOREIGN KEY (hackathon_id)
+REFERENCES hackathon(hackathon_id);
