c14277b64d9a05ca7b985013121078d96b8298d7
SamTheCoder445 <u18100156@tuks.co.za>
2026-06-26 04:07:53 +0200

    add PrimeNG user menu dropdown to navbar

--- frontend/src/app/participant/participant-shell.component/participant-shell.component.ts
+++ frontend/src/app/participant/participant-shell.component/participant-shell.component.ts
@@ -2,11 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-participant-shell',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ButtonModule, MenuModule],
   templateUrl: './participant-shell.component.html',
   styleUrls: ['./participant-shell.component.scss']
 })
@@ -14,10 +17,20 @@ export class ParticipantShellComponent implements OnInit {
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
   userName = '';
+  menuItems: MenuItem[] = [];
 
   ngOnInit(): void {
     const user = this.authService.getUser();
     this.userName = user ? `${user.firstName} ${user.lastName}` : 'Participant';
+
+    this.menuItems = [
+      {
+        label: 'Logout',
+        icon: 'pi pi-sign-out',
+        command: () => this.logout()
+      }
+    ];
+
   }
 
   logout(): void {
