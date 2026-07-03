83e4fdb1cf7adb73168c161e7a5f093041186966
REDD-debug <u23558572@tuks.co.za>
2026-07-03 09:42:57 +0200

    use PrimeNG p-button on dashboard for consistency

--- frontend/src/app/admin/components/dashboard/dashboard.component.ts
+++ frontend/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {ButtonModule} from 'primeng/button'
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,ButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
