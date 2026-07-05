edec16b49503683bcde73b730ccc3497fa719243
SamTheCoder445 <u18100156@tuks.co.za>
2026-07-05 02:30:55 +0200

    Add placeholder event data for event details banner

--- frontend/src/app/participant/event-details/event-details.component.ts
+++ frontend/src/app/participant/event-details/event-details.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-event-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.scss']
 })
+
 export class EventDetailsComponent {
+
+ event = {
+  name: 'Enetelect Hackathon 2026',
+  description:'Build amazing software solutions and collaborate with other developers.',
+  prizePool: 'R50 000',
+  startDate: '17 Aug 2026',
+  endDate: '20 Aug 2026',
+  teamSize: 4,
+  visibility: 'Public'
+};
 
 }
