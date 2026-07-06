17d9960417318ddc3c49d0f5d9393618e8a004a8
ayush-635 <ayushsanjith@gmail.com>
2026-07-06 19:29:05 +0200

    Hackathon request file created

---
+++ backend/src/main/java/com/hackathon/platform/dto/HackathonRequest.java
@@ -0,0 +1,15 @@
+package com.hackathon.platform.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+public class HackathonRequest {
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String description;
+
+}
