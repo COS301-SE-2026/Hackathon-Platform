ada6429031ebff10a7973fed9d2940e28d5899ab
ayush-635 <ayushsanjith@gmail.com>
2026-07-09 11:32:11 +0200

    update endpnt tests

--- backend/src/test/java/com/hackathon/platform/controller/HackathonControllerTest.java
+++ backend/src/test/java/com/hackathon/platform/controller/HackathonControllerTest.java
@@ -77,5 +77,13 @@ class HackathonControllerTest{
         mockMvc.perform(post("/api/hackahon").with(authentication(partic)).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(hackathonRequest))).andExpect(status().isForbidden());
     }
 
+    @Test
+    void getAllHackathons_returnSeededHackathon() throws Exception{
+        mockMvc.perform(get("/api/hackathon").with(authentication(partic))).andExpect(status().isOk()).andExpect(jsonPath("$").isArray()).andExpect(jsonPath("$[?(@.hackathonId == ' " +seededHackathonId+"')]").exists());
+    }
 
+    @Test
+    void getHackathonById_returnSeededHackathon() throws Exception {
+        mockMvc.perform(get("/api/hackathons/{hackathonId}", seededHackathonId).with(authentication(partic))).andExpect(status().isOk()).andExpect(jsonPath("$.name").value("Seeded hackathon"));
+    }
 }
