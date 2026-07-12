cc5b63cd8578882e819bdfda16603494c4bc01d1
SamTheCoder445 <u18100156@tuks.co.za>
2026-07-12 03:08:04 +0200

    added leaderboard header and current team summary

--- frontend/src/app/participant/event-details/event-details-tabs/leaderboard/leaderboard.component.ts
+++ frontend/src/app/participant/event-details/event-details-tabs/leaderboard/leaderboard.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface MyTeamStanding {
+  rank: number;
+  name: string;
+  score: number;
+}
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './leaderboard.component.html',
-  styleUrl: './leaderboard.component.css',
+  styleUrl: './leaderboard.component.scss',
 })
-export class LeaderboardTabComponent {}
+export class LeaderboardComponent {
+
+    myTeam: MyTeamStanding = {
+    rank: 4,
+    name: 'ByteForce',
+    score: 8420
+  };
+
+}
