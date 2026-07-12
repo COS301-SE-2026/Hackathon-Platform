138bc166d9d2abf1a9ce207d4bb7b40cc1aba0de
SamTheCoder445 <u18100156@tuks.co.za>
2026-07-12 14:58:56 +0200

    Add full rankings section to leaderboard

--- frontend/src/app/participant/event-details/event-details-tabs/leaderboard/leaderboard.component.ts
+++ frontend/src/app/participant/event-details/event-details-tabs/leaderboard/leaderboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
 
 interface Team {
   rank: number;
@@ -9,7 +10,7 @@ interface Team {
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss',
 })
@@ -18,18 +19,31 @@ export class LeaderboardComponent {
     myTeam: Team = {
     rank: 4,
     name: 'ByteForce',
-    score: 8420
+    score: 	9500000
   };
 
   leaderboard: Team[] = [
-  { rank: 1, name: 'Debug Thugs', score: 98420 },
-  { rank: 2, name: 'Keybord Gremlins', score: 97210 },
-  { rank: 3, name: 'Code Blooded', score: 96155 },
-  { rank: 4, name: 'ByteForce', score: 8420 },
-  { rank: 5, name: 'Null Pointers', score: 8310 }
+  { rank: 1, name: 'Debug Thugs', score: 9800000 },
+  { rank: 2, name: 'Keybord Gremlins', score: 9700000 },
+  { rank: 3, name: 'Code Blooded', score: 9600000 },
+  { rank: 4, name: 'ByteForce', score: 9500000 },
+  { rank: 5, name: 'Null Pointers', score: 9400000 }
 ];
 
 get topThree(): Team[] {
-  return this.leaderboard.slice(0, 3);
+     return this.leaderboard.slice(0, 3);
 }
+
+getInitials(name: string): string {
+
+    const words = name.trim().split(/\s+/);
+    
+    if (words.length === 1) { 
+      return words[0][0].toUpperCase();  
+    }
+    return (words[0][0] +words[words.length - 1][0]).toUpperCase();
+
+  }
+
+
 }
