f5367546ca45c75a22d5fc1ec6ce7f73fe34f71d
REDD-debug <u23558572@tuks.co.za>
2026-07-14 13:09:26 +0200

    test: configure TestBed for LevelsComponent with mocks and spies

---
+++ frontend/src/app/admin/components/levels/levels.component.spec.ts
@@ -0,0 +1,47 @@
+import { ComponentFixture,TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
+import { Router , ActivatedRoute} from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import {of} from 'rxjs';
+import { LevelsComponent} from './levels.component';
+
+describe('LevelsComponent',() => {
+    let component: LevelsComponent;
+    let fixture: ComponentFixture<LevelsComponent>;
+    let routerNavigateSpy: jasmine.Spy;
+    let activatedRouteMock: any;
+
+    const mockLevel = {
+        id: 1 , 
+        name: 'Level 1',
+        difficulty: 'Introduction',
+        scoringMode: 'highest',
+        files: ['file1.txt','file2.pdf']
+    };
+
+    
+    beforeEach(async () => {
+        activatedRouteMock ={
+            snapshot: {
+                paramMap:{
+                    get: jasmine.createSpy('get').and.returnValue('hack-123')
+                }
+            },
+            queryParams: of ({}),
+            params: of({})
+        };
+    await TestBed.configureTestingModule({
+        imports: [FormsModule,RouterTestingModule,LevelsComponent],
+        providers: [ 
+            {provide: ActivatedRoute, useValue: activatedRouteMock}
+            ]
+    
+        }).compileComponents();
+
+    routerNavigateSpy = spyOn(TestBed.inject(Router),'navigate');
+    fixture = TestBed.createComponent(LevelsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+});
+})
