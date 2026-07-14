90e53fe09e4620e05337bbf5fd944e5f0f089885
REDD-debug <u23558572@tuks.co.za>
2026-07-14 09:56:18 +0200

    configure TestBed for HackathonsComponent with mocks and spies

---
+++ frontend/src/app/admin/components/hackathons/hackathons.component.spec.ts
@@ -0,0 +1,50 @@
+import { ComponentFixture,TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
+import { Router,ActivatedRoute, RouterModule } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { MessageService } from "primeng/api";
+import {of} from 'rxjs';
+import {HackathonsComponent} from './hackathons.component';
+
+describe('HackathonsComponent', () =>{
+    let component: HackathonsComponent;
+    let fixture: ComponentFixture<HackathonsComponent>;
+    let routerNavigateSpy: jasmine.Spy;
+    let messageServiceMock: jasmine.SpyObj<MessageService>;
+    let activatedRouteMock: any;
+
+    const mockHackathon = {
+        id: 'hack-123',
+        name: 'Testing Hackathon',
+        description: 'Testing description',
+        status: 'upcoming' as const,
+        eventCount: 0
+    };
+
+    beforeEach(async () => {
+        messageServiceMock = jasmine.createSpyObj('MessageService', ['add']);
+        activatedRouteMock ={
+            snapshot: {
+                paramMap:{
+                    get: jasmine.createSpy('get').and.returnValue(null)
+                }
+            },
+            queryParams: of ({}),
+            params: of({})
+        };
+    await TestBed.configureTestingModule({
+        imports: [FormsModule,RouterTestingModule,HackathonsComponent],
+        providers: [
+            {provide: MessageService, useValue: messageServiceMock},
+            {provide: ActivatedRoute, useValue: activatedRouteMock}
+            ]
+        }).compileComponents();
+
+    routerNavigateSpy = spyOn(TestBed.inject(Router),'navigate');
+    fixture = TestBed.createComponent(HackathonsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+});
+
+});
