1db20c3a8df38ad526ad8530ffa657dd571be853
REDD-debug <u23558572@tuks.co.za>
2026-07-16 13:59:58 +0200

    fix linting error in routes file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -52,62 +52,7 @@ export const routes: Routes = [
       }
     ]
   },
-  {
-    path: 'participant',
-    component: ParticipantShellComponent,
-    children: [
-      {
-        path: 'home',
-        loadComponent: () => import('./participant/home/home.component').then(m => m.HomeComponent),
-      },
-      
-      {
-        path: 'profile',
-        loadComponent: () => import('./participant/profile/profile.component').then(m => m.ProfileComponent),
-      },
 
-         {
-        path: 'submissions',
-        loadComponent: () => import('./participant/submission-history/submissionhistory.component').then(m => m.SubmissionHistoryComponent),
-      },
-        {
-        path: 'team', 
-        loadComponent: () => import('./participant/team/team.component').then(m => m.TeamComponent),
-      },
-       {
-        path: 'submit',  
-        loadComponent: () => import('./participant/submit/submit.component').then(m => m.SubmitComponent),
-      }, 
-       {
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full',
-      }
-    ]
-  },
-  { path: '**', redirectTo: 'login' }
-import { Routes } from '@angular/router';
-import { AdminShellComponent } from './admin/components/admin-shell.component/admin-shell.component';
-import { ParticipantShellComponent } from './participant/participant-shell.component/participant-shell.component';
-import { AuthGuard } from './guards/auth.guard';
-
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
-  { path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent) },
-  {
-    path: 'admin',
-    component: AdminShellComponent,
-    canActivate: [AuthGuard],
-    children: [
-      { path: 'dashboard', loadComponent: () => import('./admin/components/dashboard/dashboard.component').then(m => m.DashboardComponent) },
-      { path: 'create-event', loadComponent: () => import('./admin/components/create-event/createEvent.component').then(m => m.CreateEventComponent) },
-      { path: 'event-list', loadComponent: () => import('./admin/components/event-list/eventlist.component').then(m => m.EventlistComponent) },
-      { path: 'events/edit/:id', loadComponent: () => import('./admin/components/manage-event/manage-event.component').then(m => m.ManageEventComponent) },
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-    ]
-  },
   {
     path: 'participant',
     component: ParticipantShellComponent,
