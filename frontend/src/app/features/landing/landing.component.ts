01c17ec285c6f12fa555393b15bc153f323aeee6
SamTheCoder445 <u18100156@tuks.co.za>
2026-07-19 03:31:19 +0200

    added landing page navbar

--- frontend/src/app/features/landing/landing.component.ts
+++ frontend/src/app/features/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LandingNavbarComponent } from './components/landing-navbar/landing-navbar.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LandingNavbarComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
