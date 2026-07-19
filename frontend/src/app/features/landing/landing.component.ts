92adde4501d503d8f4b9377fede192c1476cbe8c
SamTheCoder445 <u18100156@tuks.co.za>
2026-07-19 06:39:35 +0200

    added landing page home section

--- frontend/src/app/features/landing/landing.component.ts
+++ frontend/src/app/features/landing/landing.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LandingNavbarComponent } from './components/landing-navbar/landing-navbar.component';
+import { LandingHomeComponent } from './components/landing-home/landing-home.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule, LandingNavbarComponent],
+  imports: [CommonModule, RouterModule, LandingNavbarComponent, LandingHomeComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
