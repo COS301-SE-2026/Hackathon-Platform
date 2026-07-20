5a1cab7f72c7bf9c4473659b503a6a6aac1917b5
SamTheCoder445 <u18100156@tuks.co.za>
2026-07-20 14:11:15 +0200

    Added landing page How It Works section

--- frontend/src/app/features/landing/landing.component.ts
+++ frontend/src/app/features/landing/landing.component.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { LandingNavbarComponent } from './components/landing-navbar/landing-navbar.component';
 import { LandingHomeComponent } from './components/landing-home/landing-home.component';
 import { LandingFeaturesComponent } from './components/landing-features/landing-features.component';
+import { LandingHowItWorksComponent } from './components/landing-how-it-works/landing-how-it-works.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterModule, LandingNavbarComponent, LandingHomeComponent, LandingFeaturesComponent],
+  imports: [CommonModule, RouterModule, LandingNavbarComponent, LandingHomeComponent, LandingFeaturesComponent, LandingHowItWorksComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
