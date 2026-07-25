2f4c039d6d44a5ba8abff2f02dbc693b29699d60
REDD-debug <u23558572@tuks.co.za>
2026-07-25 16:26:30 +0200

    remove unused SafeHtml import

--- frontend/src/app/components/brand-style-guide/brand-style-guide.component.ts
+++ frontend/src/app/components/brand-style-guide/brand-style-guide.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule} from '@angular/router';
-import { DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import { DomSanitizer} from '@angular/platform-browser';
 
 
 @Component ({
