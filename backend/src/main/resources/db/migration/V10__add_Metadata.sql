71403d15ae9870ec0a6b8c9117c8916fc49db265
ayush-635 <ayushsanjith@gmail.com>
2026-07-25 15:02:52 +0200

    new migration file for missing model data

---
+++ backend/src/main/resources/db/migration/V10__add_Metadata.sql
@@ -0,0 +1,17 @@
+ALTER TABLE levelfiles
+    ADD COLUMN IF NOT EXISTS file_size BIGINT,
+    ADD COLUMN IF NOT EXISTS content_type VARCHAR(255);
+
+ALTER TABLE solverversion
+    ADD COLUMN IF NOT EXISTS version_number INT,
+    ADD COLUMN IF NOT EXISTS file_name TEXT,
+    ADD COLUMN IF NOT EXISTS file_size BIGINT,
+    ADD COLUMN IF NOT EXISTS content_type VARCHAR(255);
+
+ALTER TABLE submissions
+    ADD COLUMN IF NOT EXISTS output_file_name TEXT,
+    ADD COLUMN IF NOT EXISTS source_file_name TEXT,
+    ADD COLUMN IF NOT EXISTS output_size_size BIGINT,
+    ADD COLUMN IF NOT EXISTS source_file_size BIGINT,
+    ADD COLUMN IF NOT EXISTS output_content_type VARCHAR(255),
+    ADD COLUMN IF NOT EXISTS source_content_type VARCHAR(255);
