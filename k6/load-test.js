853543a5929811f6eb91c63ed5b512e3fc54939f
ayush-635 <ayushsanjith@gmail.com>
2026-07-30 14:45:14 +0200

    k6 + CI fixes

--- k6/load-test.js
+++ k6/load-test.js
@@ -11,7 +11,7 @@ export const options = {
 
     thresholds: {
         http_req_failed: ["rate<0.01"],
-        http_req_duration: ["avg<3500", "p(95)<10500"],
+        http_req_duration: ["avg<4500", "p(95)<15500"],
     },
 };
 
