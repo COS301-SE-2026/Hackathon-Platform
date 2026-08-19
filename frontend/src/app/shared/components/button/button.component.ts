1ea092a3e7f37326279fed8031ad5156858494b5
SamTheCoder445 <u18100156@tuks.co.za>
2026-08-19 08:08:26 +0200

    Fix accessibility issues

--- frontend/src/app/shared/components/button/button.component.ts
+++ frontend/src/app/shared/components/button/button.component.ts
@@ -8,14 +8,17 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class ButtonComponent {
-   @Output() keydown = new EventEmitter<KeyboardEvent>();
-  @Output() keyup = new EventEmitter<KeyboardEvent>();
+  
+  @Output() buttonKeydown = new EventEmitter<KeyboardEvent>();
+  @Output() buttonKeyup = new EventEmitter<KeyboardEvent>();
+
   @Input() variant: 'primary' | 'secondary' | 'danger' | 'ghost' | 'success' = 'primary';
-    onKeyDown(event: KeyboardEvent) {
-    this.keydown.emit(event);
+
+  onKeyDown(event: KeyboardEvent) {
+    this.buttonKeydown.emit(event);
   }
 
   onKeyUp(event: KeyboardEvent) {
-    this.keyup.emit(event);
+    this.buttonKeyup.emit(event);
   }
 }
