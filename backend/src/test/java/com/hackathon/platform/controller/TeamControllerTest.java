90cadd973b2276e50b7be8d9c4fc9a3c49b56ef3
ayush-635 <ayushsanjith@gmail.com>
2026-09-15 22:17:42 +0200

    join request test

--- backend/src/test/java/com/hackathon/platform/controller/TeamControllerTest.java
+++ backend/src/test/java/com/hackathon/platform/controller/TeamControllerTest.java
@@ -322,4 +322,19 @@ class TeamControllerTest {
   void getMyTeamForEvent_returns204() throws Exception{
     mockMvc.perform(get("api/teams/my-team").param("eventId", eventId.toString()).with(authentication(userAuth))).andExpect(status().isNoContent());
   }
+
+  @Test
+  void requestToJoin_returns201() throws Exception{
+    MvcResult res = mockMvc.perform(post("api/teams").with(authentication(userAuth)).contentType(MediaType.APPLICATION_JSON).content(objMapper.writeValueAsString(createTeamRequest))).andExpect(status().isCreated()).andReturn();
+    TeamResponse team = objMapper.readValue(res.getResponse().getContentAsString(), TeamResponse.class);
+    User member = User.builder().userId(UUID.randomUUID()).firstName("Kevin").lastName("Hart").email("kv@gmail.com").passwordHash("hash").role(roleRepository.findByName("PARTICIPANT").orElse(null)).status("ACTIVE").build();
+    User saveMem = userRepository.saveAndFlush(member);
+    EventRegistration reg = new EventRegistration();
+    reg.setEventId(eventId);
+    reg.setUserId(saveMem.getUserId());
+    eventRegRepo.saveAndFlush(reg);
+
+    UsernamePasswordAuthenticationToken memberAuth = new UsernamePasswordAuthenticationToken(saveMem, null, List.of(new SimpleGrantedAuthority("ROLE_PARTICIPANT")));
+    mockMvc.perform(post("/api/teams/join/{joinCode}", team.getJoinCode().toLowerCase()).with(authentication(memberAuth))).andExpect(status().isCreated());
+  }
 }
