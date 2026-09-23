77f14b12b3a679f3e7734692ee2bd7c7611e62b2
ayush-635 <ayushsanjith@gmail.com>
2026-09-24 00:10:35 +0200

    im tired now

---
+++ backend/src/main/java/com/hackathon/platform/dto/CertificateAssetResponse.java
@@ -0,0 +1,16 @@
+package com.hackathon.platform.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class  CertificateAssetResponse{
+    private final String storageKey;
+    private final String url;
+
+    public CertificateAssetResponse(String storageKey, String url){
+        this.storageKey = storageKey;
+        this.url = url;
+    }
+}
