473ade9620f61a767e42078608f60afea78ea0ef
Jasveer Ramdhaw <Ramdhawjasveer@gmail.com>
2026-09-25 14:35:41 +0200

    Updates for sonarqube

--- ast-service/app.py
+++ ast-service/app.py
@@ -118,8 +118,6 @@ def _error_ratio(root) -> float:
             walk(child)
 
     walk(root)
-    if total == 0:
-        return 0.0
     return errors / total
 
 #Embedding semantic similarity stuff
